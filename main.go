5e8c6fefe3a99897fda3aa2513515f2bb298ce66
LufDevOps <devops.hoanghagiang@gmail.com>
2024-04-02 17:19:56 +0700

    readiness and liveesss

--- main.go
+++ main.go
@@ -3,20 +3,23 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	// "log"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
 	// Load environment variables from .env file
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Println("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 
 	// Retrieve PostgreSQL connection details from environment variables
 	host := os.Getenv("POSTGRES_HOST")
@@ -31,27 +34,54 @@ func main() {
 		host, port, user, password, dbname)
 
 	// Open a connection to the PostgreSQL database
-	conn, err := sql.Open("postgres", psqlInfo)
+	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close()
+	defer db.Close()
 
 	// Create a new Gin router
 	router := gin.Default()
 
 	// Define a route to handle GET requests to /version
-	router.GET("/version", func(c *gin.Context) {
-		// Query the database for the PostgreSQL version
-		var version string
-		err := conn.QueryRow("SELECT version();").Scan(&version)
-		if err != nil {
-			c.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-		c.JSON(200, gin.H{"version": version})
-	})
+	router.GET("/version", getVersion)
+
+	// Define a route to handle liveness probes at /healthz
+	router.GET("/healthz", livenessProbe)
+
+	// Define a route to handle readiness probes at /ready
+	router.GET("/ready", readinessProbe)
 
 	// Run the Gin router on port 8080
 	router.Run(":8080")
+}
+
+func getVersion(c *gin.Context) {
+	// Query the database for the PostgreSQL version
+	var version string
+	err := db.QueryRow("SELECT version();").Scan(&version)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"version": version})
+}
+
+func livenessProbe(c *gin.Context) {
+	// Perform a simple check to verify the health of the application
+	err := db.Ping()
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Unhealthy")
+		return
+	}
+	c.String(http.StatusOK, "OK")
+}
+
+func readinessProbe(c *gin.Context) {
+	err := db.Ping()
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Not Ready")
+		return
+	}
+	c.String(http.StatusOK, "Ready")
 }
