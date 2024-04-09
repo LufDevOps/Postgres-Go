465e170a6ccd3199268aa8bb19b22608bec3ae5a
LufDevOps <devops.hoanghagiang@gmail.com>
2024-04-09 17:44:36 +0700

    abc

--- build/migratedown.sh
+++ build/migratedown.sh
@@ -6,4 +6,4 @@ source "$(dirname "$0")/.env"
 set +a
 
 # Run migration down
-migrate -path db/migration -database "postgresql://$POSTGRES_USER:$POSTGRES_DBPASSWORD@localhost:5433/$POSTGRES_DBNAME?sslmode=disable" -verbose down
+migrate -path db/migration -database "postgresql://$POSTGRES_USER:$POSTGRES_DBPASSWORD@localhost:5432/$POSTGRES_DBNAME?sslmode=disable" -verbose down
--- build/migrateup.sh
+++ build/migrateup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo $POSTGRES_USER
-
-
+echo $POSTGRES_DBNAME
 # Run migration up
-migrate -path ./migrations -database "postgresql://$POSTGRES_USER:$POSTGRES_DBPASSWORD@localhost:5433/$POSTGRES_DBNAME?sslmode=disable" -verbose up
+migrate -path ./migration -database "postgresql://$POSTGRES_USER:$POSTGRES_DBPASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DBNAME?sslmode=disable" -verbose up
+#migrate -path ./migrations -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose up
