252af178741bdcedf3fb126459b280ae86bdf3ca
LufDevOps <devops.hoanghagiang@gmail.com>
2024-05-07 10:05:43 +0700

    wtf

--- old_build/migrateup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Postgres is up - begin migrating"
-# Run migration up
-
-#docker exec -it postgres createdb --username=postgres --owner=postgres postgres
-
-migrate -path ./migration -database "postgresql://postgres:postgrespassword@postgres:5432/shop?sslmode=disable" -verbose up
