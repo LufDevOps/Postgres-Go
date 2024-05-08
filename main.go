74a727b3f40fc79194bfcbb53ddea29aab879284
LufDevOps <devops.hoanghagiang@gmail.com>
2024-05-08 10:04:08 +0700

    add name to conatiner  prometheus

--- main.go
+++ main.go
@@ -111,10 +111,6 @@ func main() {
 
 	// Prometheus endpoint
 	router.Path("/prometheus").Handler(promhttp.Handler())
-
-	// Serving static files
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-
 	fmt.Println("Serving requests on port 9000")
 	err1 := http.ListenAndServe(":9000", router)
 	log.Fatal(err1)
